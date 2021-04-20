b0627ad2a97d4b280aa7074bbb9a4e16436a932d
Giuliano Serawop <gserawop13@gmail.com>
2021-04-19 19:03:49 -0600

    finished the parsing of saved notes

--- server.js
+++ server.js
@@ -5,20 +5,58 @@ const express = require('express')
 const path = require('path')
 
 // Port number
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+app.get("/notes", function (req, res) {
+    console.log(__dirname)
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    console.log("from api notes route")
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+
+      const arr = JSON.parse(data);
+      res.json(arr)
+    });
 })
+
 
 //html
 
+
  app.post("/api/notes", function (req, res){
      const key = req.body
+     fs.readFile("./db/db.json", (err, data) => {
+        if(err) throw err;
+        console.log(data)
+        
+        const arr = JSON.parse(data)
+        arr.push(key)
+
+        console.log(arr)
+
+        fs.writeFile("./db/db.json", JSON.stringify(arr), (err) => {
+            if(err){
+                console.log(err)
+            }
+            console.log('successful')
+        })
+    });
 })
 
-app.post()
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+  
+});
